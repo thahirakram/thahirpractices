bd2c5463dccb933cbf1abb3b0403d49a33a32558
thahirakram <thahirakram289@gmail.com>
2019-02-02 14:14:20 +0530

    khghv

---
+++ jspractice/class.js
@@ -0,0 +1,25 @@
+//<script>
+    // class polygon{
+    //     constructor (height, width) {
+    //         this.area = height * width;
+    //     }
+    // }
+    // document.write(new polygon(4,3).area);
+
+    class thahir {
+        constructor (height, width) {
+            this.height = height;
+            this.width = width;
+        }
+    }
+
+    class rishi extends thahir {
+        contructor(length) {
+            super(length, length);
+            this.name =  Square;
+    
+            
+        }
+    }
+
+//</script>
